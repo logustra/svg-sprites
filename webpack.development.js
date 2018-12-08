70f11581900d9bf002f7359bd400d2b0186df76f
logustra <faizal.putra@ralali.com>
2018-12-08 17:39:20 +0700

    update

--- webpack.development.js
+++ webpack.development.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
     mode: 'development',
     devServer: {
-        port: "4000",
+        port: "3000",
         open: true,
     }
 });
